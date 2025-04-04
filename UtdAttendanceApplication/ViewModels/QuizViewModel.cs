64122897645403598027f0bdd2112676611e9373
DHua03 <dylan31303@gmail.com>
2025-04-04 15:44:51 -0500

    Quiz viewmodel and view

---
+++ UtdAttendanceApplication/ViewModels/QuizViewModel.cs
@@ -0,0 +1,25 @@
+﻿namespace UtdAttendanceApplication.ViewModels
+{
+    public class QuizViewModel
+    {
+        public string? CourseName { get; set; }
+        public int CourseSection { get; set; }
+        public string? ProfName { get; set; }
+        public int QuizBankId { get; set; } // Links the quiz to a specific bank
+        public List<QuizQuestionViewModel> Questions { get; set; } = new List<QuizQuestionViewModel>();
+    }
+
+    public class QuizQuestionViewModel
+    {
+        public int QuestionId { get; set; }
+        public string? QuestionText { get; set; }
+        public List<QuestionOptionViewModel> Options { get; set; } = new List<QuestionOptionViewModel>();
+        public int? SelectedOptionId { get; set; } // This will store the student's selected option
+    }
+
+    public class QuestionOptionViewModel
+    {
+        public int OptionId { get; set; }
+        public string? OptionText { get; set; }
+    }
+}
